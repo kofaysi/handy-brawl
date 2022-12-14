da5722caf857dfc26867765db9027069b5064809
Milan <milan.berta@e.email>
2022-12-14 22:52:28 +0100

    feat:  set starter deck by user choice fix: several fixes

--- play_hand.py
+++ play_hand.py
@@ -1,12 +1,18 @@
 import all_cards
 # import numpy as np
 
-deck_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-deck = []
-for number in deck_numbers:
-    for card in all_cards.cards:
-        if card[0].get("number") == number:
-            deck.append(card)
+
+def rotate_card_to_face(c, f):
+    face_current = c[1][0].get("face")
+    if face_current != f:
+        rotate(c)
+        face_current = c[1][0].get("face")
+        if face_current != f:
+            flip(c)
+            face_current = c[1][0].get("face")
+            if face_current != f:
+                rotate(c)
+    return c
 
 
 def flip(c):
@@ -61,72 +67,77 @@ def deck_changed(d, d_new):
     return d[1:] != d_new[1:]
 
 
-def move(d_m, i, p):
-    d_m[i], d_m[p] = d_m[p], d_m[i]
-    return d_m
+def swap(d, i, j):
+    d[i], d[j] = d[j], d[i]
+    return d
 
 
 def move_card(d, a, r):
     ds_new = []
-    try:
+    if ' ' in a:
         t = a.split()[1]
-    except IndexError:
+    else:
         t = "any"
     m = None
     if r > 0:
         for i in reversed(range(1, r+1)):
             if t == "self":
-                if d[0][0].get("type") == d[i][0].get("type") and d[i][1][1].get("life") != "exhausted":
+                if d[0][0].get("type") == d[i][0].get("type") and d[i][1][0].get("life") != "exhausted":
                     m = i
                     break
             elif t == "enemy":
-                if d[0][0].get("type") != d[i][0].get("type") and d[i][1][1].get("life") != "exhausted":
+                if d[0][0].get("type") != d[i][0].get("type") and d[i][1][0].get("life") != "exhausted":
                     m = i
                     break
             else:
-                if d[i][1][1].get("life") != "exhausted":
+                if d[i][1][0].get("life") != "exhausted":
                     m = i
                     break
-        ds_new.append(move(d, m, 1))
+        ds_new.append(swap(d, m, 1))
     elif r < 0:
-        for i in range(1, r+1):
+        for i in range(1, abs(r)+1):
             if t == "self":
-                if d[0][0].get("type") == d[i][0].get("type") and d[i][1][1].get("life") != "exhausted":
+                if d[0][0].get("type") == d[i][0].get("type") and d[i][1][0].get("life") != "exhausted":
                     m = i
                     break
             elif t == "enemy":
-                if d[0][0].get("type") != d[i][0].get("type") and d[i][1][1].get("life") != "exhausted":
+                if d[0][0].get("type") != d[i][0].get("type") and d[i][1][0].get("life") != "exhausted":
                     m = i
                     break
             else:
-                if d[i][1][1].get("life") != "exhausted":
+                if d[i][1][0].get("life") != "exhausted":
                     m = i
                     break
-        ds_new.append(move(d, m, len(d)))
-    return ds_new
+        if m:
+            ds_new.append(swap(d[:], m, len(d)-1))
+    if ds_new:
+        return ds_new
+    else:
+        return [d[:]]
 
 
 def rotate_top(d):
     d_new = d
     d_new[0] = rotate(d[0])
-    return d_new
+    ds_new = [d_new]
+    return ds_new
 
 
 def use_shield(d, i):
     d_new = d
 
     switcher = {
-        "rotate": lambda c: rotate(c),
-        "do_nothing": lambda c: c
+        "rotate": lambda c: rotate(c)
     }
 
     action = d[i][1][0].get("shield")
-    d_new[i] = switcher.get(action)(deck[i])
+    if action:
+        d_new[i] = switcher.get(action)(deck[i])
     return d_new
 
 
 def top_card_to_bottom(d):
-    d_new = leftShift(d)
+    d_new = leftShift(d[:])
     return d_new
 
 
@@ -136,15 +147,23 @@ def hit_card(d, n):
     for i in range(1, n+1):
         if d[i][1][0].get("reaction") == "shield":
             shield_found = True
-            ds_new.append(use_shield(d, i+1))
+            ds_new.append(use_shield(d[:], i+1))
     if not shield_found:
         for i in range(1, n+1):
             if d[0][0].get("type") != d[i][0].get("type") and d[i][1][0].get("life") != "exhausted":
-                ds_new.append(deal_damage(d, i))
+                ds_new.append(deal_damage(d[:], i))
     if ds_new:
         return ds_new
     else:
         return [d]  # ds_new.append(d)
+
+
+def get_unique_items(list):
+    unique = []
+    for item in list:
+        if item not in unique:
+            unique.append(item)
+    return unique
 
 
 # noinspection PyShadowingNames
@@ -156,16 +175,16 @@ def victory_status(deck):
             status_hero += card[1][0].get("life") != "exhausted"
         else:
             status_monster += card[1][0].get("life") != "exhausted"
-        if status_hero == 0:
-            return "lost"
-        if status_monster == 0:
-            return "won"
+    if status_hero == 0:
+        return "lost"
+    if status_monster == 0:
+        return "won"
 
 
 def delay_card(d, a, p):
-    try:
+    if ' ' in a:
         t = a.split()[1]
-    except IndexError:
+    else:
         t = "any"
     ds_new = []
     m = None
@@ -181,11 +200,11 @@ def delay_card(d, a, p):
                 # if d[i][1][1].get("life") != "exhausted":
                 m = i
             if p > 0:
-                end_position = m - p * j
+                end_position = m + j
             else:
-                end_position = m + p * j
-            if len(d) >= end_position >= 1:
-                d_new = move(d, m, end_position)
+                end_position = m - j
+            if len(d) > end_position >= 1:
+                d_new = swap(d[:], m, end_position)
                 ds_new.append(d_new)
     return ds_new
 
@@ -202,7 +221,7 @@ def refresh(c):
 
 def heal_deck(d):
     ds_new = []
-    for i in range(1, len(d)+1):
+    for i in range(1, len(d)-1):
         if d[0][0].get("type") == d[i][0].get("type") and d[i][1][0].get("life") == "exhausted":
             d_new = d
             d_new[i] = refresh(d[i])
@@ -244,8 +263,7 @@ def arrow_deck(d, a):
 
 
 def play_card(deck):
-    decks = []
-    decks.append(deck)
+    decks = [deck]
     status = []
     switcher = {
         "hit": lambda d, a, n: hit_card(d, n),
@@ -260,25 +278,45 @@ def play_card(deck):
     }
     decks_new = []
     for i, row in enumerate(deck[0][1][1:]):
+        decks = [deck]
         for action in row:
             if decks_new:
                 decks = decks_new
                 decks_new = []
             for deck_i in decks:
                 action_raw = action[0]
-                decks_new.extend(switcher.get(action_raw.split()[0])(deck_i, action_raw, action[1]))
-            if decks_new:
-                for deck_i in decks_new:
-                    status = victory_status(deck_i)
-                    if status is None and \
-                            (deck_changed(deck, deck_i) or i == len(deck[0][1][1:])):
-                        # no win or defeat, deck changed, or it is the last iteration anyway
-                        try:
-                            deck_i = leftShift(deck_i)
-                            play_card(deck_i)
-                        except RecursionError as re:
-                            pass
+                decks_new.extend(switcher.get(action_raw.split()[0])(deck_i[:], action_raw, action[1]))
+
+            # remove duplicates and remove the original deck, if others could be created
+            decks_new = get_unique_items(decks_new[:])
+            if len(decks_new) == 1:
+                pass
+            if len(decks_new) > 1 and deck in decks_new:
+                decks_new.remove(deck)
+
+    if decks_new:
+        for deck_i in decks_new:
+            status = victory_status(deck_i)
+            deck_i_id = get_deck_ID(deck_i)
+            deck_id = get_deck_ID(deck)
+            if status is None and \
+                    (deck_changed(deck, deck_i) or i == len(deck[0][1][1:]) - 1):
+                # no win or defeat, and deck changed, or it is the last action row iteration anyway
+                try:
+                    deck_i = leftShift(deck_i)
+                    play_card(deck_i)
+                except RecursionError as re:
+                    print("Reached the max depth of recursion...")
+            else:
+                print(status, ":", deck_i)
     return status
+
+
+def get_deck_ID(d):
+    d_id = []
+    for card in d:
+        d_id.append(str(card[0].get("number")) + card[1][0].get("face"))
+    return d_id
 
 
 def print_actions(deck):
@@ -294,6 +332,21 @@ def print_actions(deck):
             print(action)
             return switcher.get(action[0])(deck)
 
+
+def get_deck(d_items):
+    deck = []
+    for number_face in d_items:
+        for card in all_cards.cards:
+            number = int(number_face[:-1])
+            face = number_face[-1]
+            if card[0].get("number") == number:
+                c_new = rotate_card_to_face(card[:], face)
+                deck.append(c_new)
+    return deck
+
+
+deck_items = ["1A", "6A", "2A", "7A", "3A", "8A", "4A", "9A", "5A"]
+deck = get_deck(deck_items)
 
 print(deck[0])
 play_card(deck)
