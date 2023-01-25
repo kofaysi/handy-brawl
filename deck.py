fe8daf290620493bc1bd1aa851cf9766d803436d
Milan <milan.berta@e.email>
2023-01-25 22:53:38 +0100

    feat: add zip_str and unzip(str) methods to the deck class feat: import re module fix: remove self.origin from __init__ causing recursion overflow

--- deck.py
+++ deck.py
@@ -2,48 +2,28 @@
 """
 # import handybrawl as hb
 import copy
+import re
 
 
-def unzip_hash(d_hash):
+def unzip(d_hash):
     """
-    process the hash char by char and collect the numbers and faces in two individual lists
+    Separates numbers and letter in the hash and creates
 
     :param d_hash: the hash of a deck
     :return: two lists, a list of numbers and a list of faces
     """
-    faces = []
-    numbers = []
-    hash_new = ''
-    # create a new has in which train of alphas are separated by a separator ' ' (blank space) from train of numerics
-    # import re is avoided on purpose. the function runs seldom.
-    for i, s in enumerate(d_hash):
-        if i > 0 and d_hash[i - 1].isalpha() ^ s.isalpha():
-            hash_new += ' '
-        hash_new += s
-
-    # split the string by the separator ' '
-    hash_list = hash_new.split(' ')
-
-    # collect str and int to separate lists
-    for item in hash_list:
-        if item.isalpha():
-            faces.append(item)
-        else:  # elif item.isnumeric()
-            numbers.append(int(item))
-    deck = []
-    for i, n in enumerate(numbers):
-        deck.append([n, faces[i]])
-    return deck
+    matches = re.findall(r'\d+|\D+', d_hash)
+    return [[int(matches[i]), matches[i + 1]] for i in range(0, len(matches) - 1, 2)]
 
 
 class Deck:
 
     def __init__(self, parameter=None):
         if isinstance(parameter, str):
-            self.deck = unzip_hash(parameter) if parameter else None
+            self.deck = unzip(parameter) if parameter else None
         if isinstance(parameter, list):
             self.deck = parameter
-        self.origin = self.deck
+        # self.origin = Deck()  # The origin can be assigned without initialisation.
         self.actions = []
 
     def __eq__(self, other):
@@ -55,17 +35,17 @@ class Deck:
 
     def __hash__(self):
         # necessary for instances to behave sanely in dicts and sets.
-        return hash(self.hash_str)
+        return hash(self.zip_str)
 
     @property
-    def hash_str(self):
+    def zip_str(self):
         """
         The method is a property. Use assignment
-            new = Deck(other.hash_str)
-        to get the hash_str for a new deck
-            new.hash_str
+            new = Deck(other.zip_str)
+        to get the zip_str for a new deck
+            new.zip_str
 
-        :return: int hash
+        :return: int zip
         """
         return ''.join([str(value) for card in self.deck for value in card])
 
